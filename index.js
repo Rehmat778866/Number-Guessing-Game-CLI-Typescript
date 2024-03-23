b9adf5b3c4f26b953820bed4206fafb40221e6f7
Rehmat778866 <rehmatkhalid11@gmail.com>
2024-03-23 16:49:15 +0500

    CLI-Number_Guessing_Game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 50 + 1);
+console.log(randomNumber);
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-50 ",
+    },
+]);
+if (answers.UserGuessedNumber === randomNumber) {
+    console.log("Congratulations! you guess right number.");
+}
+else {
+    console.log("you guessed wrong number");
+}
